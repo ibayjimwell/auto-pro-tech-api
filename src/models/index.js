b65b9bd8a9540509ab94446702f1211a89a3b6ea
ibayjimwell <ibayjimwell@gmail.com>
2026-03-30 19:11:14 +0800

    create index.js for  model

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+// enums
+export * from './enums/appointment-status.enum.js';
+
+// models
+export { Customers } from './customers.model.js';
+export { Vehicles } from './vehicles.model.js';
+export { Staff } from './staff.model.js';
+export { ServiceTypes } from './service-types.model.js';
+export { Appointments } from './appointments.model.js';
